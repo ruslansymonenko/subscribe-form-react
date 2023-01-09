7f8aec7758ec96d3968a7b495ea4d86dd843aa05
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-09 23:35:40 +0200

    made function to get input value

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import './Form.css'
 
 class Form extends React.Component {
+  state = {
+    firstName: ''
+  }
 
+  handleInput = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
 
   render() {
+    const {firstName} = this.state
+
     return <div className='subscribeForm'>
-      
+      <input 
+        className='input' 
+        type="text" 
+        placeholder='name'
+        name='firstName'
+        value={firstName}
+        onChange={this.handleInput}
+        />
+      <input 
+        className='input' 
+        type="text" 
+        placeholder='email'
+        />
+      <input 
+        className='inputCheckbox' 
+        type="checkbox" />
+      <button 
+        className='btn'>
+          Send
+      </button>
     </div>
   }
 }
