56ced8f0fa45f1922c82b3399471c6dc8a07490f
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-10 15:38:15 +0200

    made validation for name and email

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,15 +3,34 @@ import './Form.css'
 
 class Form extends React.Component {
   state = {
-    firstName: ''
+    firstName: '',
+    email: '',
+    isAgreeWithTerms: false
   }
 
   handleInput = (event) => {
     this.setState({[event.target.name]: event.target.value})
   }
 
+  handleCheckbox = (event) => {
+    this.setState({isAgreeWithTerms: event.target.checked})
+  }
+
+  validateName = () => {
+    if (this.state.firstName.length < 5) {
+      alert('Please, enter longer name');
+    }
+  }
+
+  validateMail = () => {
+    if(!(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email))
+    ) {
+      alert('Your email is not valid!');
+    };
+  }
+
   render() {
-    const {firstName} = this.state
+    const {firstName, email} = this.state
 
     return <div className='subscribeForm'>
       <input 
@@ -21,15 +40,25 @@ class Form extends React.Component {
         name='firstName'
         value={firstName}
         onChange={this.handleInput}
+        onBlur={this.validateName}
         />
       <input 
         className='input' 
         type="text" 
         placeholder='email'
+        name='email'
+        value={email}
+        onChange={this.handleInput}
+        onBlur={this.validateMail}
         />
-      <input 
-        className='inputCheckbox' 
-        type="checkbox" />
+        <div className='ckeckBoxContainer'>
+          <input 
+          className='inputCheckbox' 
+          type="checkbox" 
+          onChange={this.handleCheckbox}
+          />
+          <label>I agree with terms and conditions</label>
+        </div>
       <button 
         className='btn'>
           Send
